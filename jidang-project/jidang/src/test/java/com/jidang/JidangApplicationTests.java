72f8cdee7ae85105c219e46f8f0d0a9361d1cba7
surcrious1 <kimes1923@gmail.com>
2025-09-23 19:27:45 +0900

    기본 프로젝트

---
+++ jidang-project/jidang/src/test/java/com/jidang/JidangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jidang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JidangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
