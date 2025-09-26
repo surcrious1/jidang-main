ae000979d59fa2099c3b4027d5d25fe8cfb29ba5
Beaeze <ddong9275@naver.com>
2025-09-26 17:35:14 +0900

    db연결하고 post,comments 테이블 생성
    post는 게시물, comments는 댓글 저장할 용도

---
+++ jidang-project/jidang/src/main/java/com/jidang/Post.java
@@ -0,0 +1,34 @@
+package com.jidang;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<Comments> commentsList;
+
+}
